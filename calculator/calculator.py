5b2c9b0324fe91935fbb21f546a2ca3a1c1c6db8
dragoπ <75088766+DragonFighter603@users.noreply.github.com>
2023-07-13 17:14:56 +0200

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,10 +1,14 @@
 import math
+from copy import deepcopy
 
-g = globals()
-del g['__builtins__'].open
-del g['__builtins__'].exec
-del g['__builtins__'].eval
-del g['__builtins__'].input
+g = {}
+exec("print('Welcome to the calculator!')", g, {})
+eval = g['__builtins__']["eval"]
+input = g['__builtins__']["input"]
+del g['__builtins__']["open"]
+del g['__builtins__']["exec"]
+del g['__builtins__']["eval"]
+del g['__builtins__']["input"]
 
 while True:
     try:
